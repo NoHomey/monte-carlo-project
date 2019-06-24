ff13db0740c7bbbd4f5db749e71fbd23ee0b4270
Ivo Stratev <ivo.stratev.tues@gmail.com>
2019-06-24 21:51:46 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,26 @@
+from random import uniform, vonmisesvariate
+from sys import float_info
+import math
+
+class Generator:
+    def __init__(self, left, right = float_info.max):
+        self.left = left
+        self.right = right
+
+    def generate(self):
+        return uniform(self.left, self.right)
+
+positive = Generator(float_info.epsilon, float_info.max)
+
+unboud = Generator(float_info.min, float_info.max)
+
+absolute = Generator(0, float_info.max)
+
+radian = Generator(0, 2 * math.pi)
+
+inverse_trigonometric = Generator(-1, 1)
+
+arccosh = Generator(1, float_info.max)
+
+def union(genA, genB):
+    return Generator(max(genA.left, genB.left), min(genA.right, genB.right))
