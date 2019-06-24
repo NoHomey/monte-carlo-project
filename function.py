18407675e6ea3a0c063fcc97f6df851b6e3f1286
Valentin Varbanov <valentin.varbanov@gmail.com>
2019-06-24 23:37:45 +0300

    improve debugging

--- function.py
+++ function.py
@@ -2,9 +2,16 @@ import generator
 import math
 
 class Function:
-    def __init__(self, generator, function):
-        self.generator = generator
-        self.function  = function
+    def __init__(self, generator, function, description = ""):
+        self.generator   = generator
+        self.function    = function
+        self.description = description
+
+    def __str__(self):
+        return self.description
+
+    def __repr__(self):
+        return self.__str__()
 
 class Power(Function):
     def __init__(self, r):
@@ -14,38 +21,38 @@ class Exponential(Function):
     def __init__(self, a):
         super().__init__(generator.unboud, lambda x: math.pow(a, x))
 
-sqrt    = Function(generator.absolute, lambda x: math.sqrt(x))
+sqrt    = Function(generator.absolute, lambda x: math.sqrt(x), "√(x)")
 
-ln      = Function(generator.positive, lambda x: math.log(x))
+ln      = Function(generator.positive, lambda x: math.log(x), "ln(x)")
 
-log2    = Function(generator.positive, lambda x: math.log2(x))
+log2    = Function(generator.positive, lambda x: math.log2(x), "log2(x)")
 
-log10   = Function(generator.positive, lambda x: math.log10(x))
+log10   = Function(generator.positive, lambda x: math.log10(x), "log10(x)")
 
-exp     = Function(generator.unboud, lambda x: math.exp(x))
+exp     = Function(generator.unboud, lambda x: math.exp(x), "e^x")
 
-sin     = Function(generator.radian, lambda x: math.sin(x))
+sin     = Function(generator.radian, lambda x: math.sin(x), "sin(x)")
 
-cos     = Function(generator.radian, lambda x: math.cos(x))
+cos     = Function(generator.radian, lambda x: math.cos(x), "cos(x)")
 
-tan     = Function(generator.radian, lambda x: math.tan(x))
+tan     = Function(generator.radian, lambda x: math.tan(x), "tan(x)")
 
-arcsin  = Function(generator.inverse_trigonometric, lambda x: math.asin(x))
+arcsin  = Function(generator.inverse_trigonometric, lambda x: math.asin(x), "arcsin(x)")
 
-arccos  = Function(generator.inverse_trigonometric, lambda x: math.acos(x))
+arccos  = Function(generator.inverse_trigonometric, lambda x: math.acos(x), "arccos(x)")
 
-arctan  = Function(generator.unboud, lambda x: math.atan(x))
+arctan  = Function(generator.unboud, lambda x: math.atan(x), "arctan(x)")
 
-sinh    = Function(generator.unboud, lambda x: math.sinh(x))
+sinh    = Function(generator.unboud, lambda x: math.sinh(x), "sinh(x)")
 
-cosh    = Function(generator.unboud, lambda x: math.cosh(x))
+cosh    = Function(generator.unboud, lambda x: math.cosh(x), "cosh(x)")
 
-tanh    = Function(generator.unboud, lambda x: math.tanh(x))
+tanh    = Function(generator.unboud, lambda x: math.tanh(x), "tanh(x)")
 
-arcsinh = Function(generator.unboud, lambda x: math.asinh(x))
+arcsinh = Function(generator.unboud, lambda x: math.asinh(x), "arcsinh(x)")
 
-arccosh = Function(generator.arccosh, lambda x: math.acosh(x))
+arccosh = Function(generator.arccosh, lambda x: math.acosh(x), "arccosh(x)")
 
-arctanh = Function(generator.inverse_trigonometric, lambda x: math.atanh(x))
+arctanh = Function(generator.inverse_trigonometric, lambda x: math.atanh(x), "arctanh(x)")
 
 positive_id = Function(generator.absolute, lambda x: x)
--- matrix_entry.py
+++ matrix_entry.py
@@ -2,3 +2,9 @@ class MatrixEntry:
     def __init__(self, coeficient, function):
         self.coeficient = coeficient
         self.function   = function
+
+    def __str__(self):
+        return str(self.coeficient) + self.function.__str__()
+
+    def __repr__(self):
+        return self.__str__()
