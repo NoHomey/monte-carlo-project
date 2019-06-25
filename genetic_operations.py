73e4d653411cd4b02470b6ba9e6d3c7736c34e8b
NoHomey <ivo.stratev.tues@gmail.com>
2019-06-25 22:45:52 +0300

    adding mutation

--- genetic_operations.py
+++ genetic_operations.py
@@ -1,14 +1,29 @@
 from function_matrix import evaluate
 
+def __generate_values__(count, generators):
+    count = len(generators)
+    values = []
+    for i in range(count):
+        values.append(generators[i].generate())
+    return values
+
 def generate(matrix, generators, creator, target):
-    rows = len(matrix[0])
+    return __create_individual__(matrix, __generate_values__, generators, creator, target)
+
+def __mutate_values__(values, index, generators):
+    new_values = values.copy()
+    new_values[index] = generators[index].generate()
+
+def mutate(matrix, generators, values, index, creator, target):
+    values_creator = lambda generators: __mutate_values__(values, index, generators)
+    return __create_individual__(matrix, values_creator, generators, creator, target)
+
+def __create_individual__(matrix, values_creator, generators, individual_creator, target):
     while True:
         try:
-            values = []
-            for i in range(rows):
-                values.append(generators[i].generate())
+            values = values_creator(generators)
             result = evaluate(matrix, values)
-            return creator(values, result, target)
+            return individual_creator(values, result, target)
         except:
             # overflow error, ignore and try again
             pass
