a184b394d374ca32ab29f44d6d018ab3c9804aec
Valentin Varbanov <valentin.varbanov@gmail.com>
2019-06-26 23:28:39 +0300

    Fix some errors that caused items to be dropped from the list

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,28 @@
+import heapq
+from typing import List
+
 from genetic_operations import GeneticOperations
 
 
-def main_algorithm(min_population : int, max_population: int, num_iterations : int, start_tolerance : float,
-                   genetic_operations : GeneticOperations):
+def main_algorithm(min_population: int, max_population: int, num_iterations: int, start_tolerance: float,
+                   genetic_operations: GeneticOperations):
 
     decreasing_step_tolerance = start_tolerance / num_iterations
 
     current_tolerance = start_tolerance
 
-    initial_population = create_initial_population(genetic_operations, min_population)
-
-    
+    initial_population: List = create_initial_population(genetic_operations, min_population)
 
 
-def create_initial_population(genetic_operations : GeneticOperations, size : int):
+def create_initial_population(genetic_operations: GeneticOperations, size: int) -> List:
 
-    return [genetic_operations.generate() for x in range(size)]
+    population: List = [genetic_operations.generate() for _ in range(size)]
+
+    return population
+
+
+def select_best_individuals(population: List, number_of_individuals: int) -> List:
+
+    population.sort()
+
+    return [population.pop(0) in range(number_of_individuals)]
--- individual.py
+++ individual.py
@@ -1,11 +1,18 @@
+
 class Individual:
+
     def __init__(self, values, score):
         self.values = values
         self.score = score
 
+    def __lt__(self, other):
+        return self.score < other.score
+
+
 def create_individual(values, result, distance_function, target):
     score = distance_function(result, target)
     return Individual(values, score)
+
 
 def individual_creator(distance_function):
     return lambda values, result, target: create_individual(values, result, distance_function, target)
