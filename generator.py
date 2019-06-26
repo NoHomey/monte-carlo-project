37cabba91b0c1756d6d9893475547d395513595c
NoHomey <ivo.stratev.tues@gmail.com>
2019-06-26 08:51:42 +0300

    allowing generated numbers to be limited in range

--- generator.py
+++ generator.py
@@ -3,29 +3,50 @@ import math
 
 __epsilon__ = 0.0000000001
 
-__max__ = 10000000000
-
-__min__ = -__max__
-
 class Generator:
-    def __init__(self, left, right = __max__):
+    max_value = 10000000000000
+    min_value = -max_value
+
+    @staticmethod
+    def set_limits(min_value, max_value):
+        Generator.min_value = min_value
+        Generator.max_value = max_value
+
+    def __init__(self, left, right = "max"):
         self.left = left
         self.right = right
 
     def generate(self):
-        return uniform(self.left, self.right)
+        left = Generator.min_value if self.left == "min" else self.left
+        right = Generator.max_value if self.right == "max" else self.right
+        return uniform(left, right)
 
-positive = Generator(__epsilon__, __max__)
+positive = Generator(__epsilon__, "max")
 
-unboud = Generator(__min__, __max__)
+unboud = Generator("min", "max")
 
-absolute = Generator(0, __max__)
+absolute = Generator(0, "max")
 
 radian = Generator(0, 2 * math.pi)
 
 inverse_trigonometric = Generator(-1, 1)
 
-arccosh = Generator(1, __max__)
+arccosh = Generator(1, "max")
 
 def intersect(genA, genB):
-    return Generator(max(genA.left, genB.left), min(genA.right, genB.right))
+    return Generator(__max__(genA.left, genB.left), __min__(genA.right, genB.right))
+
+def __extreme__(tmp, func, a, b):
+    if a == tmp:
+        return b
+    else:
+        if b == tmp:
+            return a
+        else:
+            return func(a, b)
+
+def __max__(a, b):
+    return __extreme__("min", max, a, b)
+
+def __min__(a, b):
+    return __extreme__("max", min, a, b)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from relation_symbol import RelationSymbol
 from genetic_operations import GeneticOperations
 from distance import distance
 from individual import individual_creator
+from generator import Generator
 
 matrix = [
     [MatrixEntry(2, function.sin), MatrixEntry(3, function.Power(3))],
@@ -24,6 +25,8 @@ creator = individual_creator(distance(2))
 evaluate = function_matrix.evaluate(matrix)
 
 genetic_operation = GeneticOperations(evaluate, generators, creator, target)
+
+Generator.set_limits(-100, 100)
 
 gen = genetic_operation.generate()
 
